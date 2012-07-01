7e3c29db67165281d92d276541718ac44b29adcf
Matt Wein <mwein2009@gmail.com>
2012-06-30 23:49:25 -0700

    increased tabbing to 4 spaces on the server side

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 //Splash Page
 //  - Join a new game
 // move javascript to client.js
-
 var constants = { court: { width: 600, height: 600 }, 
                   paddle: { width: 50, height: 15, delta: 3 },
                   ball: { radius: 10, deltaLeft: 3, deltaTop: 2, interval: 15 }
@@ -24,79 +23,80 @@ var app = require('http').createServer(handler),
 app.listen(80);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-    function readfile_callback(err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-      }
-      res.writeHead(200);
-      res.end(data);
-    }
-  );
+    fs.readFile(__dirname + '/index.html',
+        function readfile_callback(err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        }
+    );
 };
 
-function updateBallPosition() {
-  var newBallLeft = positions.ball.left + constants.ball.deltaLeft;
-  var newBallTop = positions.ball.top + constants.ball.deltaTop;
-  if (newBallLeft >= constants.court.width) {
-    newBallLeft = constants.court.width;
-    constants.ball.deltaLeft = -constants.ball.deltaLeft;
-  } else if (newBallLeft <= 0) {
-    newBallLeft = 0;
-    constants.ball.deltaLeft = -constants.ball.deltaLeft;
-  }
-  if (newBallTop + constants.ball.radius >= constants.court.height - constants.paddle.height) {
-    if ( (newBallLeft > ( (positions.paddles.position/100) * 600 - constants.paddle.width / 2) ) &&
-       (newBallLeft < ( (positions.paddles.position/100) * 600 + constants.paddle.width / 2) ) ) {
-        newBallTop = constants.court.height - constants.paddle.height - constants.ball.radius;
-        constants.ball.deltaTop = -constants.ball.deltaTop;
-    } else {
-       //TODO: #1
-       newBallLeft = constants.court.width / 2;
-       newBallTop = constants.court.height / 2;       
+function calculateBallPosition() {
+    var left = positions.ball.left + constants.ball.deltaLeft;
+    var top = positions.ball.top + constants.ball.deltaTop;
+    
+    if (left >= constants.court.width) {
+        left = constants.court.width;
+        constants.ball.deltaLeft = -constants.ball.deltaLeft;
+    } else if (left <= 0) {
+        left = 0;
+        constants.ball.deltaLeft = -constants.ball.deltaLeft;
     }
-  } else if (newBallTop <= 0) {
-    newBallTop = 0;
-    constants.ball.deltaTop = -constants.ball.deltaTop;
-  }
-  positions.ball.left = newBallLeft;
-  positions.ball.top = newBallTop;
+    if (top + constants.ball.radius >= constants.court.height - constants.paddle.height) {
+        if ( (left > ( (positions.paddles.position/100) * 600 - constants.paddle.width / 2) ) &&
+           (left < ( (positions.paddles.position/100) * 600 + constants.paddle.width / 2) ) ) {
+            top = constants.court.height - constants.paddle.height - constants.ball.radius;
+            constants.ball.deltaTop = -constants.ball.deltaTop;
+        } else {
+            //TODO: #1
+            left = constants.court.width / 2;
+            top = constants.court.height / 2;       
+        }
+    } else if (top <= 0) {
+        top = 0;
+        constants.ball.deltaTop = -constants.ball.deltaTop;
+    }
+    positions.ball.left = left;
+    positions.ball.top = top;
 };
 
 serverState.intervalId = setInterval( function(){
-  updateBallPosition();
+    calculateBallPosition();
 }, constants.ball.interval );  
 
 io.sockets.on('connection', function (socket) {
-  serverState.connections++;
-  console.log(serverState.connections);
-  socket.emit('environment', { court:  {  width:  constants.court.width, 
-                                          height: constants.court.height,
-                                       }, 
-                               paddle: {  position: positions.paddles.position,
-                                          width: constants.paddle.width, 
-                                          height: constants.paddle.height,
-                                          delta: constants.paddle.delta
-                                       },
-                               ball: { radius: constants.ball.radius }       
-                             } 
-              );          
-            
-  setInterval( function(){
-    socket.emit('ball', { position: { left: positions.ball.left, top: positions.ball.top } }); 
-  }, constants.ball.interval );  
-  
-  socket.on('paddle', function (data) {
-    positions.paddles.position = data.left;
-  });
-  
-  socket.on('disconnect', function () {
-    serverState.connections--;
-    if ( serverState.connections == 0 ) {
-        clearInterval( serverState.intervalId );
-        serverState.intervalId = 0;
-    }
-    console.log('player left');
-  });  
+    serverState.connections++;
+    console.log(serverState.connections);
+    socket.emit('environment', { court:  {  width:  constants.court.width, 
+                                            height: constants.court.height,
+                                         }, 
+                                 paddle: {  position: positions.paddles.position,
+                                            width: constants.paddle.width, 
+                                            height: constants.paddle.height,
+                                            delta: constants.paddle.delta
+                                         },
+                                 ball: { radius: constants.ball.radius }       
+                               } 
+                );          
+              
+    setInterval( function(){
+        socket.emit('ball', { position: { left: positions.ball.left, top: positions.ball.top } }); 
+    }, constants.ball.interval );  
+    
+    socket.on('paddle', function (data) {
+        positions.paddles.position = data.left;
+    });
+    
+    socket.on('disconnect', function () {
+        serverState.connections--;
+        if ( serverState.connections == 0 ) {
+            clearInterval( serverState.intervalId );
+            serverState.intervalId = 0;
+        }
+        console.log('player left');
+    });  
 });
