69f6a471d2dd31ee36c899cf16e46875efe11238
Matt Wein <mwein2009@gmail.com>
2012-07-01 02:12:11 -0700

    now assigning sides to the players

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ var constants = { court: { width: 600, height: 600 },
                   ball: { radius: 10, deltaLeft: 3, deltaTop: 2, interval: 30 }
                 };                         
 
-var positions = { paddles: {},
-                  ball: { left: 0, top: 0 }, 
-                };
+var state = { paddles: {},
+              ball: { left: 0, top: 0 },
+              bottomPaddle: 0,
+              topPaddle: 0,
+              leftPaddle: 0,
+              rightPaddle: 0
+            };
        
 var serverState = { intervalId: 0, 
                     connections: 0
@@ -38,8 +42,8 @@ function handler (req, res) {
 };
 
 function calculateBallPosition() {
-    var left = positions.ball.left + constants.ball.deltaLeft;
-    var top = positions.ball.top + constants.ball.deltaTop;
+    var left = state.ball.left + constants.ball.deltaLeft;
+    var top = state.ball.top + constants.ball.deltaTop;
 
     if (left >= constants.court.width) {
         left = constants.court.width;
@@ -49,10 +53,15 @@ function calculateBallPosition() {
         constants.ball.deltaLeft = -constants.ball.deltaLeft;
     }
     if (top + constants.ball.radius >= constants.court.height - constants.paddle.height) {
-        if ( (left > ( (positions.paddles.position/100) * 600 - constants.paddle.width / 2) ) &&
-           (left < ( (positions.paddles.position/100) * 600 + constants.paddle.width / 2) ) ) {
-            top = constants.court.height - constants.paddle.height - constants.ball.radius;
-            constants.ball.deltaTop = -constants.ball.deltaTop;
+        if (state.bottomPaddle) {
+            console.log(state.bottomPaddle);
+            var leftPositionOfBottomPaddle = state.paddles[state.bottomPaddle];
+            console.log(leftPositionOfBottomPaddle);
+            if (left > ( (leftPositionOfBottomPaddle/100) * 600 - constants.paddle.width / 2) &&
+                (left < ( (leftPositionOfBottomPaddle/100) * 600 + constants.paddle.width / 2) ) ) {
+              top = constants.court.height - constants.paddle.height - constants.ball.radius;
+              constants.ball.deltaTop = -constants.ball.deltaTop;
+            }
         } else {
             //TODO: #1
             left = constants.court.width / 2;
@@ -62,14 +71,28 @@ function calculateBallPosition() {
         top = 0;
         constants.ball.deltaTop = -constants.ball.deltaTop;
     }
-    positions.ball.left = left;
-    positions.ball.top = top;
+    state.ball.left = left;
+    state.ball.top = top;
 };
 
 io.sockets.on('connection', function (socket) {
 
-    positions.paddles[socket.id] = 50;
-
+    var paddleAdded = false;
+    if (!state.bottomPaddle) {
+        state.bottomPaddle = socket.id;
+    } else if (!state.topPaddle) {
+        state.topPaddle = socket.id;
+    } else if (!state.leftPaddle) {
+        state.leftPaddle = socket.id;
+    } else if (!state.rightPaddle) {
+        state.rightPaddle = socket.id;
+    } else {
+      // placeholder for fifth player
+      return;
+    }
+    
+    state.paddles[socket.id] = 50;
+    
     socket.emit('environment', { court:  {  width:  constants.court.width, 
                                             height: constants.court.height,
                                          }, 
@@ -88,18 +111,19 @@ io.sockets.on('connection', function (socket) {
     }
     
     socket.intervalId = setInterval( function(){
-        socket.emit('ball', { position: { left: positions.ball.left, top: positions.ball.top } }); 
-        socket.emit('paddles', { positions: positions.paddles });
+        socket.emit('ball', { position: { left: state.ball.left, top: state.ball.top } }); 
+        socket.emit('paddles', { positions: state.paddles });
     }, constants.ball.interval );  
     
     socket.on('paddle', function (data) {
-        positions.paddles[socket.id] = data.left;
+        state.paddles[socket.id] = data.left;
     });
     
     socket.on('disconnect', function () {
         serverState.connections--;
         clearInterval( socket.intervalId );
-        delete positions.paddles[socket.id];
+        delete state.paddles[socket.id];
+        // todo: need to give back the side so someone else can take it.
         if ( serverState.connections == 0 ) {
             clearInterval( serverState.intervalId );
             serverState.intervalId = 0;
